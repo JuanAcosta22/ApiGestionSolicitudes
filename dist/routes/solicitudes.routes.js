2a4d32ab12493b2dd8e8afb672b8ee07ea8ad7f3
JuanAcosta22 <jdacosta005@misena.edu.co>
2022-10-05 21:48:17 -0500

    Se modifica el package.json

---
+++ dist/routes/solicitudes.routes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = require("express");
+
+var _solicitudes = require("../controllers/solicitudes.controllers");
+
+var router = (0, _express.Router)(); //Obtener solicitudes
+
+router.get('/solicitudes', _solicitudes.getSolicitudes); //Crear solicitudes
+
+router.post('/solicitudes', _solicitudes.addSolicitud); //Obtener solicitudes por id
+
+router.get('/solicitudes/:id', _solicitudes.getSolicitudById); //Eliminar solicitudes
+
+router["delete"]('/solicitudes/:id', _solicitudes.deleteSolicitudById); //Editar solicitud
+
+router.put('/solicitudes/:id', _solicitudes.updateSolicitudAsignadaById);
+var _default = router;
+exports["default"] = _default;
