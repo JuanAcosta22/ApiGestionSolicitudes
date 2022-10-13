cc3a94732d61670f3293c3d4e4f6b202489d1fe4
JuanAcosta22 <jdacosta005@misena.edu.co>
2022-10-12 22:23:35 -0500

    Modificacion radicado

--- src/database/querys.js
+++ src/database/querys.js
@@ -5,7 +5,7 @@ export const queries = {
     'insert into Solicitud ('+
     'Radicado, Descripcion, FechaSolicitud, FechaRespuesta, IDResponsable, CorreoSolicitante, NombreSolicitante, ApellidoSolicitante, '+
     'TelefonoSolicitante, NombreEmpresa, IdTipoSolicitud, IDEstado)' +
-    'values (SUBSTRING((SELECT TipoSolicitud from TipoSolicitud where IDTipo=@IdTipoSolicitud),1,3) + (SELECT (CONVERT(VARCHAR,GETDATE(),112) + CONVERT(VARCHAR,(SELECT MAX(IDSolicitud) FROM Solicitud)+1))), '+
+    'values (SUBSTRING((SELECT TipoSolicitud from TipoSolicitud where IDTipo=@IdTipoSolicitud),1,3) + select(CONVERT(VARCHAR,GETDATE(),112) + CONVERT(VARCHAR,DATEPART(hh, getdate()))+CONVERT(VARCHAR,DATEPART(n, getdate()))+CONVERT(VARCHAR,DATEPART(s, getdate()))+CONVERT(VARCHAR,DATEPART(ns, getdate()))), '+
     '@Descripcion, GETDATE(), NULL, NULL, @CorreoSolicitante, @NombreSolicitante, @ApellidoSolicitante, @TelefonoSolicitante, @NombreEmpresa, @IdTipoSolicitud, 1)'
     ,getSolicitudById: 'select * from Solicitud where IDSolicitud = @id'
     ,deleteSolicitudById: 'delete from Solicitud where IDSolicitud = @id'
