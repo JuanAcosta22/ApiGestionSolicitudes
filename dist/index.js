e52f723cad62da638f601d8c033e8ef39c9bf481
JuanAcosta22 <jdacosta005@misena.edu.co>
2022-10-20 16:58:41 -0500

    CambioBD

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-_app["default"].listen(_app["default"].get('port'));
-
-console.log('Conexion exitosa al servidor por el puerto', _app["default"].get('port'));
