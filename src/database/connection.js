f903bf56e65d1c65b93f141058ee3eea7ffded31
JuanAcosta22 <jdacosta005@misena.edu.co>
2022-10-22 20:37:28 -0500

    Agregado del proyecto

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,26 +1,26 @@
 import sql from 'mssql';
 
   const dbConfig = {
-//    user: 'jdaqui',
-//    password: 'Valeteamo22*',
-//    server: 'localhost', 
-//    database: 'gestionsolicitudes',
-//    options: { 
-//      trustServerCertificate: true,
-//    }
-      user: 'jovenesmvm',
-      password: '0^618Bjt37y@',
-      database: 'sqldb-gestionsolicitudes-pdn-001',
-      server: 'sql-gestionsolicitudes-pdn-001.database.windows.net',
-      pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-      },
-       options: {
-       encrypt: true,
-       trustServerCertificate: false
-      }
+    user: 'jdaqui',
+    password: 'Valeteamo22*',
+    server: 'localhost', 
+    database: 'gestionsolicitudes',
+    options: { 
+      trustServerCertificate: true,
+    }
+      // user: 'jovenesmvm',
+      // password: '0^618Bjt37y@',
+      // database: 'sqldb-gestionsolicitudes-pdn-001',
+      // server: 'sql-gestionsolicitudes-pdn-001.database.windows.net',
+      // pool: {
+      //   max: 10,
+      //   min: 0,
+      //   idleTimeoutMillis: 30000
+      // },
+      //  options: {
+      //  encrypt: true,
+      //  trustServerCertificate: false
+      // }
 };
 
 export async function getConnection() {
--- src/database/querys.js
+++ src/database/querys.js
@@ -1,16 +1,22 @@
 
 
 export const queries = {
-    getAllSolicitudes: 'SELECT * FROM Solicitud'
+     getAllSolicitudes: 
+     `SELECT * FROM Solicitud`
     ,addNewSolicitud: 
-    'INSERT INTO Solicitud ('+
-    'Radicado, Descripcion, FechaSolicitud, FechaRespuesta, IDResponsable, CorreoSolicitante, NombreSolicitante, ApellidoSolicitante, '+
-    'TelefonoSolicitante, NombreEmpresa, IdTipoSolicitud, IDEstado)' +
-    'VALUES (SUBSTRING((SELECT TipoSolicitud FROM TipoSolicitud WHERE IDTipo=@IdTipoSolicitud),1,3) + (SELECT (CONVERT(VARCHAR,GETDATE(),112)) + (SELECT (CONVERT(VARCHAR,DATEPART(hh, GETDATE())))) + (SELECT(CONVERT(VARCHAR,DATEPART(n, GETDATE())))) + (SELECT(CONVERT(VARCHAR,DATEPART(s, GETDATE())))) + (SELECT(CONVERT(VARCHAR,DATEPART(ms, GETDATE())))) ), '+
-    '@Descripcion, (SELECT CONVERT(VARCHAR, GETDATE(), 23)), NULL, NULL, @CorreoSolicitante, @NombreSolicitante, @ApellidoSolicitante, @TelefonoSolicitante, @NombreEmpresa, @IdTipoSolicitud, 1)'
-    ,getSolicitudById: 'SELECT * FROM Solicitud WHERE IDSolicitud = @id'
-    ,deleteSolicitudById: 'DELETE FROM Solicitud WHERE IDSolicitud = @id'
-    ,updateSolicitudAsignadaById: 'UPDATE Solicitud SET IDResponsable = @IDResponsable, IDEstado = 2 WHERE IDSolicitud = @id'
-    ,getTipoSolicitud: 'SELECT * FROM TipoSolicitud'
+    `INSERT INTO Solicitud 
+    (Radicado, Descripcion, FechaSolicitud, FechaRespuesta, IDResponsable, CorreoSolicitante, NombreSolicitante, ApellidoSolicitante, TelefonoSolicitante, NombreEmpresa, IdTipoSolicitud, IDEstado)
+    VALUES 
+    (SUBSTRING((SELECT TipoSolicitud FROM TipoSolicitud WHERE IDTipo=@IdTipoSolicitud),1,3) + (SELECT (CONVERT(VARCHAR,GETDATE(),112)) + (SELECT (CONVERT(VARCHAR,DATEPART(hh, GETDATE())))) 
+    + (SELECT(CONVERT(VARCHAR,DATEPART(n, GETDATE())))) + (SELECT(CONVERT(VARCHAR,DATEPART(s, GETDATE())))) + (SELECT(CONVERT(VARCHAR,DATEPART(ms, GETDATE())))) ),
+    @Descripcion, (SELECT CONVERT(VARCHAR, GETDATE(), 23)), NULL, NULL, @CorreoSolicitante, @NombreSolicitante, @ApellidoSolicitante, @TelefonoSolicitante, @NombreEmpresa, @IdTipoSolicitud, 1)`
+    ,getSolicitudById: 
+    'SELECT * FROM Solicitud WHERE IDSolicitud = @id'
+    ,deleteSolicitudById: 
+    'DELETE FROM Solicitud WHERE IDSolicitud = @id'
+    ,updateSolicitudAsignadaById: 
+    'UPDATE Solicitud SET IDResponsable = @IDResponsable, IDEstado = 2 WHERE IDSolicitud = @id'
+    ,getTipoSolicitud: 
+    'SELECT * FROM TipoSolicitud'
 }
 
